c4f21992452c5998b5a923f35f0d4a1d873d0a8e
Gwen <Gwendolyn.forde@mail.citytech.cuny.edu>
2019-09-06 20:24:42 -0400

    Created project file and made scripts for move side to side and in a circle. Feel free to change the objects.

---
+++ MovingShapes/Assets/moveRightLeft.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveRightLeft : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float changingXPos;
+    float amp = 3f;
+    float freq = 1.3f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //sine + cosine function = sin/cos(frequency)*amplitude
+        //frequency = speed
+        //amplitude = distance
+        changingXPos = Mathf.Cos(Time.time * freq) * amp;
+
+        transform.position = new Vector3(changingXPos,0,0);
+    }
+}
---
+++ MovingShapes/Assets/moveinCircle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveinCircle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float changingYPos;
+    float changingXPos;
+    float amp = 5f;
+    float freq = 2f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        changingXPos = Mathf.Cos(Time.time * freq) * amp;
+        changingYPos = Mathf.Sin(Time.time * freq) * amp;
+
+        transform.position = new Vector3(changingXPos,changingYPos,0);
+    }
+}
