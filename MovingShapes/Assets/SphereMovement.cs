fd772055684ddfc1973e7e5fe4c2eb6ed84e8601
Anthony <anthony.rivera3@mail.citytech.cuny.edu>
2019-09-07 22:21:19 -0400

    Script for Sphere & Cylinder added
    Script for Sphere & Cylinder added and materials added

---
+++ MovingShapes/Assets/SphereMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMovement : MonoBehaviour
+{
+
+
+    int randomXPositionThatIsLeftOrRight;
+
+    int spinX = 5;
+    int spinY = 10;
+    int spinZ = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveTheSphereCloserToTheScreen();
+        SpinSphereInPlace();
+
+
+    }
+
+    void MoveTheSphereCloserToTheScreen()
+    {
+        transform.position = transform.position + Vector3.back * Time.deltaTime * 5;
+
+    }
+
+    void SpinSphereInPlace()
+    {
+        transform.Rotate(spinX, spinY, spinZ);
+
+    }
+}
